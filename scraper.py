76758d154c23bb76cdf634cf31a4d8afaa6d5049
Alan Covarrubias <alan.cov@icloud.com>
2020-07-07 10:59:21 -0700

    scrape for teams players and games

---
+++ scraper.py
@@ -0,0 +1,86 @@
+import numpy as np
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from operator import methodcaller
+import os
+import re
+
+
+class Scraper:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        options = ChromeDriverManager().install()
+        self.driver = webdriver.Chrome(options, options=chrome_options)
+
+    def get(self, endpoint):
+        url = os.path.join('https://www.basketball-reference.com', endpoint)
+        self.driver.get(url)
+
+    def get_teams(self, year):
+        self.get(f'leagues/NBA_{year}_standings.html')
+        teams_table = self.driver.find_element_by_id(
+            'all_team_vs_team').find_element_by_tag_name('tbody')
+        rows = teams_table.find_elements_by_tag_name('tr')
+
+        def get_team(row):
+            team = {}
+            cells = row.find_elements_by_tag_name('td')
+            text_array = cells[0].text.split()
+            if text_array[-1] == "Blazers":
+                team['name'] = ' '.join(text_array[1:])
+                team['city'] = text_array[0]
+            else:
+                team['name'] = text_array[-1]
+                team['city'] = ' '.join(text_array[:-1])
+            team['abbr'] = self.__get_team_abbr(cells[0])
+            return team
+        return [get_team(row) for row in rows]
+
+    def get_players(self, year, team):
+        self.get(f'teams/{team}/{year}.html')
+        players_table = self.driver.find_element_by_id(
+            'roster').find_element_by_tag_name('tbody')
+        rows = players_table.find_elements_by_tag_name('tr')
+
+        def get_player(row):
+            cells = row.find_elements_by_tag_name('td')
+            player = {}
+            player['name'] = cells[0].text
+            player['abbr'] = cells[0].get_attribute('data-append-csv')
+            player['position'] = cells[1].text
+            return player
+
+        return [get_player(row) for row in rows]
+
+    def get_games(self, year):
+        months = ('October', 'November', 'December', 'January',
+                  'February', 'March', 'April', 'June')
+
+        def get_month_games(month):
+            self.get(f'leagues/NBA_{year}_games-{month.lower()}.html')
+            games_table = self.driver.find_element_by_id(
+                'schedule').find_element_by_tag_name('tbody')
+            rows = games_table.find_elements_by_css_selector(
+                'tr:not(.thead)')
+
+            def get_game(row):
+                date = row.find_element_by_tag_name('th').text
+                date_array = list(map(methodcaller('strip'), date.split(',')))
+                cells = row.find_elements_by_tag_name('td')
+                game = {}
+                game['year'] = int(date_array[-1])
+                game['month'] = month
+                game['day'] = date_array[1].split()[-1]
+                game['away_team'] = self.__get_team_abbr(cells[1])
+                game['home_team'] = self.__get_team_abbr(cells[3])
+                return game
+
+            return [get_game(row) for row in rows]
+
+        month_games = [get_month_games(month) for month in months]
+        return np.concatenate(month_games)
+
+    def __get_team_abbr(self, cell): return cell.find_element_by_tag_name(
+        'a').get_attribute('href').split('/')[-2]
