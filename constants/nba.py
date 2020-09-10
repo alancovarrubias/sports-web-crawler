3cde844a939d3bd3ff17f02fe4f30cca470b2233
Alan Covarrubias <alan.cov@icloud.com>
2020-09-09 20:18:58 -0700

    gs

--- constants/nba.py
+++ constants/nba.py
@@ -1,12 +1,1 @@
 BASKETBALL_REFERENCE = 'https://www.basketball-reference.com'
-MONTHS = (
-    {'text': 'october', 'numeric': 10},
-    {'text': 'november', 'numeric': 11},
-    {'text': 'december', 'numeric': 12},
-    {'text': 'january', 'numeric': 1},
-    {'text': 'february', 'numeric': 2},
-    {'text': 'march', 'numeric': 3},
-    {'text': 'april', 'numeric': 4},
-    {'text': 'may', 'numeric': 5},
-    {'text': 'june', 'numeric': 6}
-)
--- scrapers/nba/games.py
+++ scrapers/nba/games.py
@@ -1,29 +1,33 @@
 import re
-from constants.nba import MONTHS
 from scrapers.abstract import AbstractScraper
 from scrapers.helpers import get_table_rows, get_team_abbr
-from operator import methodcaller
 
 
+NBA_MONTHS = (
+    'october',
+    'november',
+    'december',
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+)
 class NbaGamesScraper(AbstractScraper):
     def get_resource(self):
         games = []
-        for month in MONTHS:
+        for month in NBA_MONTHS:
             season = self.key_store.args['season']
-            month_text = month['text']
-            endpoint = f'leagues/NBA_{season}_games-{month_text}.html'
+            endpoint = f'leagues/NBA_{season}_games-{month}.html'
             css_selectors = ('#schedule',)
             games_table = self.get_tables(endpoint, css_selectors)[0]
             css_config = {'rows': 'tr:not(.thead)', 'cells': 'th, td'}
             table_rows = get_table_rows(games_table, css_config)
 
             def get_game(row):
-                date = row[0].text
-                date_array = list(map(methodcaller('strip'), date.split(',')))
                 game = {}
-                game['year'] = int(date_array[-1])
-                game['month'] = month['numeric']
-                game['day'] = int(date_array[1].split()[-1])
+                game['date'] = row[0].get_attribute('csk')[:-4]
                 game['away_team'] = get_team_abbr(row[2])
                 game['home_team'] = get_team_abbr(row[4])
                 return game
