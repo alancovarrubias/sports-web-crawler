80a6b7e22dd9df043da841c0d0c6e7a251b32ab3
Alan Covarrubias <alan.cov@icloud.com>
2020-09-20 18:24:49 -0700

    fix stats

--- crawlers/nba/stats.py
+++ crawlers/nba/stats.py
@@ -20,16 +20,17 @@ class NbaStatsScraper(AbstractScraper):
         away_tables = stat_tables[:2]
         home_tables = stat_tables[2:]
 
-        def get_team_stat(tables):
+        def get_team_stat(team, tables):
             basic_stats_table, advanced_stats_table = tables
             css_config = {'section': 'tfoot', 'cells': 'th, td'}
             basic_row = get_table_rows(basic_stats_table, css_config)[0]
             advanced_row = get_table_rows(advanced_stats_table, css_config)[0]
             row = basic_row + advanced_row
-            team_stat = NbaStat('Team', row)
-            return team_stat.toJson()
+            team_stat = NbaStat('Team', row).toJson()
+            team_stat['abbr'] = team
+            return team_stat
 
-        def get_player_stats(tables):
+        def get_player_stats(team, tables):
             basic_stats_table, advanced_stats_table = tables
             css_config = {'rows': 'tr:not(.thead)', 'cells': 'th, td'}
             basic_rows = get_table_rows(basic_stats_table, css_config)
@@ -39,12 +40,13 @@ class NbaStatsScraper(AbstractScraper):
                 if len(basic_row) <= 2:
                     continue
                 row = basic_row + advanced_row
-                player_stat = NbaStat('Player', row)
-                player_stats.append(player_stat.toJson())
+                player_stat = NbaStat('Player', row).toJson()
+                player_stat['team'] = team
+                player_stats.append(player_stat)
             return player_stats
 
-        away_player_stats = get_player_stats(away_tables)
-        home_player_stats = get_player_stats(home_tables)
-        away_team_stat = get_team_stat(away_tables)
-        home_team_stat = get_team_stat(home_tables)
+        away_player_stats = get_player_stats(away_team, away_tables)
+        home_player_stats = get_player_stats(home_team, home_tables)
+        away_team_stat = get_team_stat(away_team, away_tables)
+        home_team_stat = get_team_stat(home_team, home_tables)
         return {'away_player_stats': away_player_stats, 'home_player_stats': home_player_stats, 'away_team_stat': away_team_stat, 'home_team_stat': home_team_stat}
